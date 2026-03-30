1d2ed5f95c69770ba6fbdb5ee8dfd7461908994a
Abubkar Ibrahim <abubakaribrahim30012@gmail.com>
2026-03-30 23:24:33 +0100

    ad

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -44,6 +44,7 @@
         <meta name="twitter:image" content="{{ asset('images-removebg-preview.png') }}">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         @routes
